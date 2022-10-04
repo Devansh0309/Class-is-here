56a5094a9f28b93730e69f749783a37cf45942e5
gitskr26hub <kumarsugam26@gmail.com>
2022-10-05 01:26:06 +0530

    css by sugam

--- isLogged.js
+++ isLogged.js
@@ -1,7 +1,9 @@
 function isLogged(link,fileKey){
     console.log(link)
     if(localStorage.getItem('Logged')=='yes'){
+    
         link.setAttribute('href',JSON.parse(localStorage.getItem('Image Address'))[fileKey])
+       
     }
     else{
         //popup with alert and direct to signup/login
@@ -9,7 +11,7 @@ function isLogged(link,fileKey){
         document.body.style.opacity='0.5'
         
         // document.getElementById('popup').style.opacity='1'
-        // window.location.assign('LogIn.html')
+         
         link.setAttribute('href','')
     }
 }
